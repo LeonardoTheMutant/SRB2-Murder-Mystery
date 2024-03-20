e70606fd5168a4bb04639cd403392ea57d7b5f4a
LeonardoTheMutant <olekseilvanov435@gmail.com>
2024-03-20 11:21:49 +0100

    Add language sorting for `MMLANG` command

--- TOOLS/init_ramdisk_linux.sh
+++ TOOLS/init_ramdisk_linux.sh
@@ -3,8 +3,8 @@
 
 if ! [ -d /media/ramdisk ]; then
 	sudo mkdir /media/ramdisk
-	sudo mount -t tmpfs -o size=64M tmpfs /media/ramdisk #64M = 64 Megabytes, change this number to something else if needed
-	echo "Ramdisk created at /media/ramdisk"
-else
-	echo "Ramdisk is already initialized at /media/ramdisk"
 fi
+
+#64M = 64 Megabytes, change this number to something else if needed
+sudo mount -t tmpfs -o size=64M tmpfs /media/ramdisk
+echo "Ramdisk is (re)initialized at /media/ramdisk"
